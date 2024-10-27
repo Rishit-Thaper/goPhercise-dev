38af0060c97c9c11b1e51677912e17f9c3a62087
Rishit-Thaper <rishitthaper5329@gmail.com>
2024-10-27 23:27:06 +0530

    added the timer functionality to understand the concurrency and go routines

--- Gophercise-Quiz/quiz.go
+++ Gophercise-Quiz/quiz.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 )
 
 type problem struct {
@@ -28,14 +29,41 @@ func main() {
 	}
 	problems := createProblems(lines)
 	var count int = 0
+
 	for i, problem := range problems {
 		fmt.Printf("Problem #%d: %s\n", i+1, problem.q)
-		var answer string
-		fmt.Scanln(&answer)
-		if answer == problem.a {
-			count = count + 1
-		} else {
-			break
+
+		//channel creation to get data
+		answerCh := make(chan string)
+
+		//concurrent function Go-routine with Anonymous function
+
+		// Goroutine: Jab go keyword se function call kiya jata hai, 
+		// to ye function alag thread mein execute hota hai, 
+		// jisse main program ka flow rukta nahi hai.
+		go func() {
+			var answer string
+			//blocking statement and a seperate goroutine to get the input
+			fmt.Scanln(&answer)
+			answerCh <- answer
+		}()
+		select {
+			//get the data from channel, blocking statement
+		case answer := <-answerCh:
+			{
+				if answer == problem.a {
+					count = count + 1
+				} else {
+					fmt.Printf("Incorrect Answer")
+					break
+				}
+			}
+			//another concurrent function without hampering the actual flow tracking time that returns a channel
+		case <-time.After(15 * time.Second):
+			{
+				fmt.Printf("Times Up!")
+				break
+			}
 		}
 	}
 	fmt.Printf("Your final score is %d out of %d.\n", count, len(problems))
