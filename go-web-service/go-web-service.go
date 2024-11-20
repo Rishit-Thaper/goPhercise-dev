11191d88236c858dcc033465842600d9175bdfca
Rishit-Thaper <rishitthaper5329@gmail.com>
2024-11-21 00:02:52 +0530

    DB connectivity

--- go-web-service/go-web-service.go
+++ go-web-service/go-web-service.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
+	"os"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 type User struct {
@@ -13,6 +17,13 @@ type User struct {
 	Name  string `json:"name"`
 	Email string `json:"email"`
 	Age   int    `json:"age"`
+}
+
+type DBConfig struct {
+	Username string
+	Password string
+	DBUrl    string
+	DBName   string
 }
 
 var users = []User{
@@ -47,6 +58,42 @@ var users = []User{
 		Email: "ethan@test.com",
 		Age:   35,
 	},
+}
+
+func getDBConfig() DBConfig {
+	return DBConfig{
+		Username: getEnvVariables("DB_USERNAME"),
+		Password: getEnvVariables("DB_PASSWORD"),
+		DBUrl:    getEnvVariables("DB_URL"),
+		DBName:   getEnvVariables("DB_NAME"),
+	}
+}
+
+func getEnvVariables(key string) string {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+	value := os.Getenv(key)
+	return value
+}
+
+func connectToDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+
+	if err != nil {
+		fmt.Println("120err", err)
+		panic(err)
+	}
+	err = db.Ping()
+	fmt.Println("124err", err)
+
+	if err != nil {
+		fmt.Println("127err", err)
+		panic(err)
+	}
+	fmt.Print("Pong\n")
+	return db, nil
 }
 
 func getUsers(c *gin.Context) {
@@ -113,6 +160,18 @@ func deletetUser(c *gin.Context) {
 func main() {
 	fmt.Println("Go Web Service")
 	router := gin.Default()
+
+	dbConfig := getDBConfig()
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s", dbConfig.Username, dbConfig.Password, dbConfig.DBUrl, dbConfig.DBName)
+
+	db, err := connectToDB(dsn)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
 	router.GET("/users", getUsers)
 	router.POST("/add-users", addUsers)
 	router.PUT("/edit-users/:userId", editUser)
